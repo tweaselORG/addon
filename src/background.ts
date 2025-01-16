c2daa3c6637873a09485501154fcf24e74c7e29e
Benjamin Altpeter <hi@bn.al>
2025-01-16 14:59:23 +0100

    HAR recording

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,5 @@
 import { parse as parseCookieHeader, parseSetCookie as parseSetCookieHeader } from 'cookie-es';
-import type { Entry } from 'har-format';
+import type { Entry, Har } from 'har-format';
 import * as qs from 'qs-esm';
 import { ExtensionMessageParams } from './util/message';
 import { generateReference, httpHeadersToHarHeaders, pause } from './util/util';
@@ -10,7 +10,12 @@ browser.runtime.onInstalled.addListener(async () => {
 
 browser.action.onClicked.addListener(() => browser.tabs.create({ url: '/ui.html' }));
 
+type RequestId = string;
+
 const analyzeWebsite = async (options: { url: string; reference: string }) => {
+    const manifest = browser.runtime.getManifest();
+    const browserInfo = await browser.runtime.getBrowserInfo();
+
     const container = await browser.contextualIdentities.create({
         name: `tweasel-temp-${options.reference}`,
         color: 'toolbar',
@@ -25,79 +30,72 @@ const analyzeWebsite = async (options: { url: string; reference: string }) => {
     if (!tab.id) throw new Error('Could not create tab.');
     // await browser.tabs.hide(tab.id);
 
-    const storageKey = (item: string, requestId: string) => `tmp-${tab.id}-${requestId}-${item}`;
+    const onBeforeRequestEvents: Record<RequestId, browser.webRequest._OnBeforeRequestDetails> = {};
+    const onSendHeadersEvents: Record<RequestId, browser.webRequest._OnSendHeadersDetails> = {};
+    const onBeforeRedirectEvents: Record<RequestId, browser.webRequest._OnBeforeRedirectDetails> = {};
+    const onResponseStartedEvents: Record<RequestId, browser.webRequest._OnResponseStartedDetails> = {};
+
+    const har: Har = {
+        log: {
+            version: '1.2',
+            creator: {
+                name: manifest.name,
+                version: manifest.version,
+            },
+            browser: {
+                name: `${browserInfo.vendor} ${browserInfo.name}`,
+                version: `${browserInfo.version} (${browserInfo.buildID})`,
+            },
+            pages: [],
+            entries: [],
+        },
+    };
 
     browser.webRequest.onBeforeRequest.addListener(
-        async (details) => {
-            await browser.storage.local.set({ [storageKey('onBeforeRequest', details.requestId)]: details });
-
-            return { cancel: false };
+        (details) => {
+            onBeforeRequestEvents[details.requestId] = details;
         },
         { urls: ['<all_urls>'], tabId: tab.id },
         ['requestBody'],
     );
 
     browser.webRequest.onSendHeaders.addListener(
-        async (details) => {
-            await browser.storage.local.set({ [storageKey('onSendHeaders', details.requestId)]: details });
-
-            return { cancel: false };
+        (details) => {
+            onSendHeadersEvents[details.requestId] = details;
         },
         { urls: ['<all_urls>'], tabId: tab.id },
         ['requestHeaders'],
     );
 
     browser.webRequest.onBeforeRedirect.addListener(
-        async (details) => {
-            await browser.storage.local.set({ [storageKey('onBeforeRedirect', details.requestId)]: details });
-
-            return { cancel: false };
+        (details) => {
+            onBeforeRedirectEvents[details.requestId] = details;
         },
         { urls: ['<all_urls>'], tabId: tab.id },
     );
 
     browser.webRequest.onResponseStarted.addListener(
-        async (details) => {
-            await browser.storage.local.set({
-                [storageKey('onResponseStarted-timestamp', details.requestId)]: details.timeStamp,
-            });
-
-            return { cancel: false };
+        (details) => {
+            onResponseStartedEvents[details.requestId] = details;
         },
         { urls: ['<all_urls>'], tabId: tab.id },
     );
 
     browser.webRequest.onCompleted.addListener(
         async (onCompletedDetails) => {
-            const onBeforeRequestKey = storageKey('onBeforeRequest', onCompletedDetails.requestId);
-            const onBeforeRequestDetails: browser.webRequest._OnBeforeRequestDetails = (
-                await browser.storage.local.get(onBeforeRequestKey)
-            )[onBeforeRequestKey];
-            await browser.storage.local.remove(onBeforeRequestKey);
+            const onBeforeRequestDetails = onBeforeRequestEvents[onCompletedDetails.requestId];
+            const onSendHeadersDetails = onSendHeadersEvents[onCompletedDetails.requestId];
+            const onBeforeRedirectDetails = onBeforeRedirectEvents[onCompletedDetails.requestId];
+            const onResponseStartedDetails = onResponseStartedEvents[onCompletedDetails.requestId];
 
-            const onSendHeadersKey = storageKey('onSendHeaders', onCompletedDetails.requestId);
-            const onSendHeadersDetails: browser.webRequest._OnSendHeadersDetails = (
-                await browser.storage.local.get(onSendHeadersKey)
-            )[onSendHeadersKey];
-            await browser.storage.local.remove(onSendHeadersKey);
-
-            const onBeforeRedirectKey = storageKey('onBeforeRedirect', onCompletedDetails.requestId);
-            const onBeforeRedirectDetails: browser.webRequest._OnBeforeRedirectDetails = (
-                await browser.storage.local.get(onBeforeRedirectKey)
-            )[onBeforeRedirectKey];
-            await browser.storage.local.remove(onBeforeRedirectKey);
-
-            const onResponseStartedTimestampKey = storageKey(
-                'onResponseStarted-timestamp',
-                onCompletedDetails.requestId,
-            );
-            const onResponseStartedTimestamp: number = (await browser.storage.local.get(onResponseStartedTimestampKey))[
-                onResponseStartedTimestampKey
-            ];
-            await browser.storage.local.remove(onResponseStartedTimestampKey);
+            delete onBeforeRequestEvents[onCompletedDetails.requestId];
+            delete onSendHeadersEvents[onCompletedDetails.requestId];
+            delete onBeforeRedirectEvents[onCompletedDetails.requestId];
+            delete onResponseStartedEvents[onCompletedDetails.requestId];
 
             if (!onBeforeRequestDetails) throw new Error('Missing onBeforeRequestDetails');
             if (!onSendHeadersDetails) throw new Error('Missing onSendHeadersDetails');
+            if (!onResponseStartedDetails) throw new Error('Missing onResponseStartedDetails');
 
             // See: https://www.w3.org/Protocols/rfc2616/rfc2616-sec6.html#sec6.1
             const [responseHttpVersion, , responseStatusText] = onCompletedDetails.statusLine.split(' ');
@@ -110,21 +108,28 @@ const analyzeWebsite = async (options: { url: string; reference: string }) => {
             const requestMimeType =
                 requestHeaders.find((h) => h.name.toLowerCase() === 'content-type')?.value ||
                 'application/octet-stream';
-            let requestBodyText = onBeforeRequestDetails.requestBody?.raw?.[0]?.bytes;
-            if (
-                !requestBodyText &&
-                onBeforeRequestDetails.requestBody?.formData &&
-                requestMimeType === 'application/x-www-form-urlencoded'
-            )
-                requestBodyText = qs.stringify(onBeforeRequestDetails.requestBody.formData);
+            const requestBodyParams = onBeforeRequestDetails.requestBody?.formData
+                ? Object.entries(onBeforeRequestDetails.requestBody?.formData!).flatMap(([name, value]) =>
+                      value.map((v: unknown) => ({
+                          name,
+                          value: v?.toString(),
+                      })),
+                  )
+                : undefined;
+            // TODO: What does it mean if `raw` has more than one entry? How should we handle that?
+            let requestBodyText = onBeforeRequestDetails.requestBody?.raw?.[0]?.bytes
+                ? new TextDecoder().decode(onBeforeRequestDetails.requestBody?.raw?.[0]?.bytes)
+                : undefined;
+            if (!requestBodyText && requestBodyParams && requestMimeType === 'application/x-www-form-urlencoded')
+                requestBodyText = qs.stringify(Object.fromEntries(requestBodyParams.map((p) => [p.name, p.value])));
 
             const responseMimeType =
                 responseHeaders.find((h) => h.name.toLowerCase() === 'content-type')?.value ||
                 'application/octet-stream';
 
             const send = onSendHeadersDetails.timeStamp - onBeforeRequestDetails.timeStamp;
-            const wait = onResponseStartedTimestamp - onSendHeadersDetails.timeStamp;
-            const receive = onCompletedDetails.timeStamp - onResponseStartedTimestamp;
+            const wait = onResponseStartedDetails.timeStamp - onSendHeadersDetails.timeStamp;
+            const receive = onCompletedDetails.timeStamp - onResponseStartedDetails.timeStamp;
 
             const entry: Entry = {
                 startedDateTime: new Date(onBeforeRequestDetails.timeStamp).toISOString(),
@@ -156,16 +161,8 @@ const analyzeWebsite = async (options: { url: string; reference: string }) => {
                             // According to the spec, `params` and `text` are mutually exclusive, however that is just
                             // not true in practice, cf.:
                             // https://github.com/tweaselORG/TrackHAR/issues/58#issuecomment-1838215940
-                            params: onBeforeRequestDetails.requestBody?.formData
-                                ? Object.entries(onBeforeRequestDetails.requestBody?.formData!).flatMap(
-                                      ([name, value]) =>
-                                          value.map((v: unknown) => ({
-                                              name,
-                                              value: v?.toString(),
-                                          })),
-                                  )
-                                : undefined,
-                            text: requestBodyText,
+                            params: requestBodyParams as undefined,
+                            text: requestBodyText as string,
                         },
                     }),
                     headersSize: -1,
@@ -203,7 +200,8 @@ const analyzeWebsite = async (options: { url: string; reference: string }) => {
                 time: send + wait + receive,
                 serverIPAddress: onCompletedDetails.ip,
             };
-            console.log(entry);
+
+            har.log.entries.push(entry);
         },
         { urls: ['<all_urls>'], tabId: tab.id },
         ['responseHeaders'],
@@ -211,8 +209,10 @@ const analyzeWebsite = async (options: { url: string; reference: string }) => {
 
     await browser.tabs.update(tab.id, { url: options.url });
 
-    await pause(5000);
+    await pause(60000);
 
+    await browser.tabs.remove(tab.id);
+    console.log(JSON.stringify(har));
     await browser.contextualIdentities.remove(container.cookieStoreId);
 };
 
@@ -241,11 +241,4 @@ browser.action.onClicked.addListener(() => {
 
         browser.tabs.reload();
     });
-});
-
-browser.tabs.onRemoved.addListener(async (tabId) => {
-    // There is no `getKeys()` function, so we always have to pointlessly fetch all this data. :(
-    const allKeys = Object.keys(await browser.storage.local.get());
-
-    await browser.storage.local.remove(allKeys.filter((key) => key.startsWith(`tmp-${tabId}-`)));
 });
